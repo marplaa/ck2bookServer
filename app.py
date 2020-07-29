9ba65a2b584b190be8ffa20f0f22e591c24bee78
Unknown <martin.plaas+github@gmail.com>
2020-07-29 08:45:22 +0200

    add gitignore

---
+++ app.py
@@ -0,0 +1,140 @@
+import hashlib
+import os
+import re
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify
+from pip._vendor import urllib3
+from flask import request
+import wget
+from pathlib import Path
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route('/get/get_recipe_data_json_get', methods=['GET'])
+def get_recipe_data_json_get():
+    url = request.args.get('url');
+    print(url)
+    recipe_data = get_recipe_data(url)
+
+    return jsonify(recipe_data)
+
+
+def get_recipe_data(url):
+    recipe = {"url": url}
+    soup = soupify(url)
+    content = soup.find("div", {"class": "print__content_left"}).find("p")
+    # print(soup.find("div", {"class": "ds-box"}).get_text())
+
+    recipe['title'] = soup.find("a", {"class": "bi-recipe-title"}).getText()
+    try:
+        recipe['subtitle'] = soup.find("div", {"id": "content"}).find("strong").getText()
+    except Exception:
+        pass
+
+    recipe['ingredients'] = makelist(soup.find("table", {"class": "print__ingredients"}))
+
+    recipe['recipe_info'] = makelist(soup.find("table", {'id': 'recipe-info'}).extract())
+    recipe['text'] = re.sub(r'\s+', ' ', content.get_text('</br>', strip=True)).replace('\n', '')
+
+    # get images
+
+    # soup = soupify(url.replace("/drucken",""))
+    # print(soup.find("div", {"class": "recipe-image"}))
+    # print(url.replace("/drucken",""))
+    # imgs = soup.find("div", {"class": "recipe-image"}).find_all("amp-img")
+    image = soup.find("figure").find("img")
+    # for image in imgs:
+
+    try:
+        recipe['images'] = getImages(url)
+    except:
+        pass
+
+    recipe['image'] = image.get('src')
+
+    return recipe
+
+
+def soupify(url):
+    if "/drucken/" not in url:
+        url = url.replace("/rezepte/", "/rezepte/drucken/")
+
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+
+    return BeautifulSoup(response.data, 'html.parser')
+
+
+# todo check if images überhaupt vorhanden sind unter bilderübersicht
+def getImages(url):
+    url = url.replace("/rezepte/", "/rezepte/bilderuebersicht/")
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    soup = BeautifulSoup(response.data, 'html.parser')
+    # print(soup.find("div", {'class': 'recipe-images'}))
+    images = soup.find("div", {'class': 'recipe-images'}).findAll('amp-img')
+    img_list = []
+    # print(images)
+
+    for img in images:
+        img_list.append(img.get('src'))
+
+    print(len(img_list))
+    # download_images(img_list)
+
+    return img_list
+
+
+@app.route('/compile/toPdf', methods=['POST'])
+def create_tex_file():
+    data = request.get_json()
+    directory = hashlib.md5(bytearray(data['content'], encoding="utf-8")).hexdigest()
+
+    try:
+        Path(directory).mkdir()
+        Path(directory + '/images').mkdir()
+
+    except FileExistsError:
+        pass
+
+    print(Path('/book.txt'))
+    print()
+
+    f = open(Path(directory) / 'book.tex', "w", encoding='utf-8')
+    f.write(data['content'])
+    f.close()
+
+    download_images(data['images'], Path(directory) / 'images')
+
+    print(data)
+    return {'url': 'ggsdfdsfsdfsdf'}
+
+
+def download_images(images, path):
+    for image in images:
+        name = hashlib.md5(bytearray(image, encoding="ascii")).hexdigest() + '.jpg'
+        print(path / name)
+        wget.download(image, out=str(path / name))
+
+
+def makelist(table):
+    result = []
+
+    allrows = table.findAll('tr')
+    for row in allrows:
+        result.append([])
+        allcols = row.findAll('td')
+        for col in allcols:
+            thestrings = []
+            thestring = ""
+            for thestring in col.stripped_strings:
+                thestring = re.sub(r'\s+', ' ', thestring)
+                # thestring = thestring.replace('\n', '')
+                thestrings.append(thestring)
+            thetext = ''.join(thestrings)
+            result[-1].append(thetext)
+    return result
